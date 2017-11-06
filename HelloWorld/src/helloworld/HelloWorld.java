e084dd4afb16e5f42caf8acff6950d4a70acb497
montanabraswell <montanabraswell1@gmail.com>
2017-11-06 10:29:43 -0500

    Add files via upload
    Java exercise #2

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,24 @@
+/* MOOC Java Excerise 2
+ * Java Tutorial 2
+ * Montana Braswell
+ * 10/26/17.
+ */
+package helloworld;
+
+/**
+ *
+ * @author Montana
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+        System.out.print("(And all the people of the world, dont be afraid to get up and move)");
+        
+    }
+    
+}
